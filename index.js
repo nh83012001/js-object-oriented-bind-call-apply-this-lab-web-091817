3d7c8b25389b26209b2c89dac50aaf4c7e25af33
Nick Hall <nickhall122@gmail.com>
2017-11-01 10:01:29 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function justInvoke(fn){
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arg){
+  return fn.call(thisValue, arg)
+}
+
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue,args)
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  const newFunction = functionToBeCopied.bind(thisValue)
+  return newFunction
+}
